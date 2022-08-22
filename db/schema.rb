a0f36f6bcdc9f07f679288cb5e5462e74e3dfb1f
Alex <alexandre.dromel@gmail.com>
2022-08-22 16:14:15 +0200

    creating controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_135540) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_141308) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_135540) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "airport_id", null: false
+    t.bigint "aircraft_id", null: false
+    t.date "start_on"
+    t.date "end_on"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["aircraft_id"], name: "index_bookings_on_aircraft_id"
+    t.index ["airport_id"], name: "index_bookings_on_airport_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,4 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_135540) do
 
   add_foreign_key "aircrafts", "airports"
   add_foreign_key "aircrafts", "users"
+  add_foreign_key "bookings", "aircrafts"
+  add_foreign_key "bookings", "airports"
+  add_foreign_key "bookings", "users"
 end
