3eb6012af086869783ec055f6e5bdfc71284e8ed
Alex <alexandre.dromel@gmail.com>
2022-08-23 16:07:26 +0200

    Airports index + show

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -2,4 +2,9 @@ class AirportsController < ApplicationController
   def index
     @airports = Airport.all
   end
+
+  def show
+    @airport = Airport.find(params[:id])
+    @aircrafts = Aircraft.where(airport_id: @airport.id)
+  end
 end
