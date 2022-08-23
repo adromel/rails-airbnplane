125560e057acfdf136dd1d82ab4fea4ad7f291da
Francois973 <robino.francois973@gmail.com>
2022-08-23 14:02:45 +0200

    commit before test calandar

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,9 @@
 class BookingsController < ApplicationController
+  def start_time
+    self.my_related_model.start ##Where 'start' is a attribute of type 'Date' accessible through MyModel's relationship
+  end
+
+  def create
+
+  end
 end
