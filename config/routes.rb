10a3a908f1cb6b9a1e2fc8bf42f2f826c7769827
Antoine Gros <antoine.gros14@gmail.com>
2022-08-24 16:37:31 +0200

    alert nav + fb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: "pages#home"
 
   # vue de la recherche user
@@ -19,8 +19,6 @@ Rails.application.routes.draw do
   resources :aircrafts do
     resources :bookings, only: [:new, :create]
   end
-
-
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
