c76432fe2522e832523834bc35a9661b4032ebeb
Alex <alexandre.dromel@gmail.com>
2022-08-25 12:28:26 +0200

    Added confirmed column to Bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_25_090837) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_102545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_25_090837) do
     t.date "end_on"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "confirmed"
     t.index ["aircraft_id"], name: "index_bookings_on_aircraft_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
